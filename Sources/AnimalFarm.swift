5a1df01517a3b84845f242b8b1fdbfc16ec7c17b
Rafal Manka <rafal@manka.info>
2023-08-10 11:33:09 +0400

    final solution

--- Sources/AnimalFarm.swift
+++ Sources/AnimalFarm.swift
@@ -41,39 +41,98 @@ extension Farm {
         case farmName
         case animals
     }
-
+    
     enum AnimalTypeCodingKeys: String, CodingKey {
         case type
     }
-
+    
     enum AnimalTypes: String, Decodable {
         case cow, dog, fish
     }
-
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: FarmCodingKeys.self)
         farmName = try container.decode(String.self, forKey: .farmName)
-
+        
         var animalsArrayContainer = try container.nestedUnkeyedContainer(forKey: .animals)
         var animals = [Animal]()
-
+        
         while !animalsArrayContainer.isAtEnd {
-            let animalContainer = try animalsArrayContainer.nestedContainer(keyedBy: AnimalTypeCodingKeys.self)
-            let type = try animalContainer.decode(AnimalTypes.self, forKey: .type)
-
-            switch type {
-            case .cow:
-                let cow = try animalsArrayContainer.decode(Cow.self)
-                animals.append(cow)
-            case .dog:
-                let dog = try animalsArrayContainer.decode(Dog.self)
-                animals.append(dog)
-            case .fish:
-                let fish = try animalsArrayContainer.decode(Fish.self)
-                animals.append(fish)
+            let animalDict = try animalsArrayContainer.decode([String: AnyCodable].self)
+            if let typeValue = animalDict["type"]?.stringValue, let type = AnimalTypes(rawValue: typeValue) {
+                let animalData = try JSONEncoder().encode(animalDict)
+                switch type {
+                case .cow:
+                    let cow = try JSONDecoder().decode(Cow.self, from: animalData)
+                    animals.append(cow)
+                case .dog:
+                    let dog = try JSONDecoder().decode(Dog.self, from: animalData)
+                    animals.append(dog)
+                case .fish:
+                    let fish = try JSONDecoder().decode(Fish.self, from: animalData)
+                    animals.append(fish)
+                }
             }
         }
-
+        
         self.animals = animals
+    }
+}
+
+struct AnyCodable: Codable {
+    var value: Any
+
+    init<T>(_ value: T?) {
+        self.value = value as Any
+    }
+    
+    var stringValue: String {
+        return "\(value)"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let val = try? container.decode(Bool.self) {
+            self.value = val
+        } else if let val = try? container.decode(Int.self) {
+            self.value = val
+        } else if let val = try? container.decode(Double.self) {
+            self.value = val
+        } else if let val = try? container.decode(String.self) {
+            self.value = val
+        } else if let val = try? container.decode([AnyCodable].self) {
+            self.value = val.map { $0.value }
+        } else if let val = try? container.decode([String: AnyCodable].self) {
+            var dict = [String: Any]()
+            for (key, anyCodable) in val {
+                dict[key] = anyCodable.value
+            }
+            self.value = dict
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "AnyCodable value cannot be decoded")
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch self.value {
+        case let value as Bool:
+            try container.encode(value)
+        case let value as Int:
+            try container.encode(value)
+        case let value as Double:
+            try container.encode(value)
+        case let value as String:
+            try container.encode(value)
+        case let value as [Any]:
+            try container.encode(value.map { AnyCodable($0) })
+        case let value as [String: Any]:
+            let convertedDict = value.reduce(into: [String: AnyCodable]()) { $0[$1.key] = AnyCodable($1.value) }
+            try container.encode(convertedDict)
+        default:
+            throw EncodingError.invalidValue(self.value, EncodingError.Context(codingPath: [], debugDescription: "AnyCodable value cannot be encoded"))
+        }
     }
 }
