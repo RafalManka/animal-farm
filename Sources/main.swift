ee9b4062795a545076e57e28dc5d4750c3286054
Rafal Manka <rafal@manka.info>
2023-08-10 14:58:01 +0400

    improve loop casting

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,12 +7,14 @@ let farm = try! Parser.parse()
 print("Farm name: \(farm.farmName)")
 
 for animal in farm.animals {
-    print("Animal type: \(animal.type)")
-    if let cow = animal as? Cow {
-        print("Milk per day: \(cow.milkPerDay), Grass requirements: \(cow.grassRequirements)")
-    } else if let dog = animal as? Dog {
-        print("Name: \(dog.name), Speed: \(dog.speed), Command abilities: \(dog.commandAbilities.joined(separator: ", "))")
-    } else if let fish = animal as? Fish {
-        print("Weight: \(fish.weight), Swimming speed: \(fish.swimmingSpeed), Scale color: \(fish.scaleColor)")
+    switch animal {
+    case let cow as Cow:
+        print("\(cow.type): Milk per day: \(cow.milkPerDay), Grass requirements: \(cow.grassRequirements)")
+    case let dog as Dog:
+        print("\(dog.type):Name: \(dog.name), Speed: \(dog.speed), Command abilities: \(dog.commandAbilities.joined(separator: ", "))")
+    case let fish as Fish:
+        print("\(fish.type):Weight: \(fish.weight), Swimming speed: \(fish.swimmingSpeed), Scale color: \(fish.scaleColor)")
+    default:
+        break
     }
 }
